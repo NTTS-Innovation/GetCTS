5c17d0776c4740f43b348532d77a66ab632454d9
Johan Ryberg <johan.ryberg@global.ntt>
2021-06-09 08:57:33 +0000

    ifconfig should not be used anymore. Using ip instead

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ EOF
   echo "dummy" > /etc/modules-load.d/dummy.conf
   echo "options dummy numdummies=1" > /etc/modprobe.d/dummy.conf
   modprobe -v dummy numdummies=1
-  ifconfig dummy0 up
+  ip link set up dummy0
 fi
 # Install Docker
 echo ""
