62955c9893147363d7c80c9f4c6dc6635a9f0943
Johan Ryberg <johan.ryberg@global.ntt>
2021-06-18 12:49:54 +0000

    Added support for debian/ubuntu

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ format_disk() {
   echo "Available disks to partition and format for data storage:"
   for d in $disks
     do
-      if [[ $(/sbin/sfdisk -d ${d} 2>&1) == "" ]]; then
+      if [[ $(/sbin/sfdisk -d ${d} 2>&1) == ""  || $(/sbin/sfdisk -d ${d} 2>&1) == *"does not contain a recognized partition table"* ]]; then
         echo "  $d"
         unformated_disks="$d $unformated_disks"
       fi
@@ -83,7 +83,7 @@ format_disk() {
   done
   parted $disk --script mklabel msdos
   parted $disk --script mkpart primary ext4 0% 100%
-  PARTITION=$(sfdisk -d ${disk} |grep Id=83|awk '{print $1}')
+  PARTITION=$(sfdisk -d ${disk} | egrep 'Id=83|type=83' | awk '{print $1}')
   mkfs.ext4 $PARTITION
   mkdir -p /srv/docker/cts/data
   if ! grep "/srv/docker/cts/data" /etc/fstab; then
@@ -94,6 +94,23 @@ format_disk() {
   fi
   mount -a
 }
+
+# Load os-release so we know on what dist we are
+if [ -f "/etc/os-release" ]; then
+  . /etc/os-release
+  if [[ "${ID}" == "centos" ]] || [[ "${ID}" == "ubuntu" ]] || [[ "${ID}" == "debian" ]]; then
+      echo "Supported Linux dist detected: ${ID}"
+      DIST=${ID}
+  else
+      echo "Unsupported Linux dist detected: ${ID}, aborting!"
+      clean
+      exit 1
+  fi
+else
+  echo "/etc/os-release is missing, that should not happend, aborting!"
+  clean
+  exit 1
+fi
 
 # Check if /srv/docker/cts/data is a mount
 set +e
@@ -124,27 +141,39 @@ set -e
 
 # Update system and kernel
 echo "Updating operating system and kernel"
-yum clean all && yum -y update && yum -y update kernel
-
-if ! vercomp ${MIN_KERNEL_VERSION} ${RUNNING_KERNEL_VERSION}; then
-  echo "Running Kernel version is too old and the system was just updated to latest version"
-  echo "Please reboot and run this command again"
-  exit 1
+if [[ "${DIST}" == "centos" ]]; then
+  yum clean all && yum -y update && yum -y update kernel
+  if ! vercomp ${MIN_KERNEL_VERSION} ${RUNNING_KERNEL_VERSION}; then
+    echo "Running Kernel version is too old and the system was just updated to latest version"
+    echo "Please reboot and run this command again"
+    exit 1
+  fi
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  apt update && apt -y dist-upgrade
 fi
 
 # Create support user for NTT
 echo ""
-echo "Creating NTTSecurity support user"
-adduser NTTSecurity || true
-echo "Please type a temporary password for user NTTSecurity and write it down in a secure place"
+echo "Creating nttsecurity support user"
+adduser nttsecurity || true
+echo "Please type a temporary password for user nttsecurity and write it down in a secure place"
 echo "This password needs to be distributed to NTT Service transition team for management"
 echo ""
 read -s -p "Password: " CREDENTIALS
-echo ${CREDENTIALS} | passwd NTTSecurity --stdin
-usermod -aG wheel NTTSecurity
+if [[ "${DIST}" == "centos" ]]; then
+  echo ${CREDENTIALS} | passwd nttsecurity --stdin
+  usermod -aG wheel nttsecurity
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  echo "nttsecurity:${CREDENTIALS}" | chpasswd
+  usermod -aG sudo nttsecurity
+fi
 
 # Install required packages
-yum install -y ntp yum-plugin-versionlock yum-utils device-mapper-persistent-data lvm2
+if [[ "${DIST}" == "centos" ]]; then
+  yum install -y ntp yum-plugin-versionlock yum-utils device-mapper-persistent-data lvm2
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  apt -y install apt-transport-https ca-certificates curl gnupg lsb-release ntp netplan.io ntpdate
+fi
 
 # Configure NTP
 echo ""
@@ -167,59 +196,88 @@ EOF
 if [ ! -z ${NTP2} ];then
   echo "server ${NTP2} iburst" >> /etc/ntp.conf
 fi
-systemctl stop ntpd
-ntpdate ${NTP1}
-hwclock --systohc
-systemctl enable ntpd
-systemctl start ntpd
-timedatectl set-timezone UTC
+if [[ "${DIST}" == "centos" ]]; then
+  systemctl stop ntpd
+  ntpdate ${NTP1}
+  hwclock --systohc
+  systemctl enable ntpd
+  systemctl start ntpd
+  timedatectl set-timezone UTC
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  systemctl stop ntp
+  ntpdate ${NTP1}
+  hwclock --systohc
+  systemctl enable ntp
+  systemctl start ntp
+  timedatectl set-timezone UTC
+fi
 
 # Creating dummy0 interface
-# Some AWS instances of CentOS 7 hang during boot, we need a work around for them
-TOKEN=$(curl --silent --connect-timeout 2 -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600") > /dev/null 2>&1 \
-&& curl --silent --connect-timeout 2 -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ > /dev/null 2>&1
-if [[ "$?" == "0" ]] && [[ ! ${TOKEN} == *"ERR_CONNECT_FAIL"* ]]; then
-  echo "This is most likely an EC2 instance in AWS. Creating dummy0 using rc.local"
-  cat <<EOF > /etc/rc.local
+if [[ "${DIST}" == "centos" ]]; then
+  # Some AWS instances of CentOS 7 hang during boot, we need a work around for them
+  TOKEN=$(curl --silent --connect-timeout 2 -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600") > /dev/null 2>&1 \
+  && curl --silent --connect-timeout 2 -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ > /dev/null 2>&1
+  if [[ "$?" == "0" ]] && [[ ! ${TOKEN} == *"ERR_CONNECT_FAIL"* ]]; then
+    echo "This is most likely an EC2 instance in AWS. Creating dummy0 using rc.local"
+    cat <<EOF > /etc/rc.local
 modprobe dummy numdummies=1
 ip link set name dummy0 dev dummy0
 ip link set dummy0 up
 EOF
-  chmod +x /etc/rc.d/rc.local
-  systemctl enable rc-local
-  modprobe dummy numdummies=1
-  ip link set name dummy0 dev dummy0
-  ip link set dummy0 up
-else
-  echo "Creating dummy0 interface"
-  cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-dummy0
+    chmod +x /etc/rc.d/rc.local
+    systemctl enable rc-local
+    modprobe dummy numdummies=1
+    ip link set name dummy0 dev dummy0
+    ip link set dummy0 up
+  else
+    echo "Creating dummy0 interface"
+    cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-dummy0
 DEVICE=dummy0
 NM_CONTROLLED=no
 ONBOOT=yes
 TYPE=Ethernet
 EOF
-  echo "dummy" > /etc/modules-load.d/dummy.conf
-  echo "options dummy numdummies=1" > /etc/modprobe.d/dummy.conf
-  modprobe -v dummy numdummies=1
-  ip link set up dummy0
+    echo "dummy" > /etc/modules-load.d/dummy.conf
+    echo "options dummy numdummies=1" > /etc/modprobe.d/dummy.conf
+    modprobe -v dummy numdummies=1
+    ip link set up dummy0
+  fi
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  cat <<EOF > /etc/netplan/10-cts-dummy0.yaml
+network:
+  bridges:
+    dummy0:
+      optional: true
+      dhcp4: false
+EOF
+  netplan generate
+  netplan apply
+  echo "Sleeping for 10 seconds to make sure all interfaces has been reloaded"
+  sleep 10
 fi
 # Install Docker
 echo ""
 echo "Removing unwanted packages, error messages may occur"
-sudo yum remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
-
-yum-config-manager \
+if [[ "${DIST}" == "centos" ]]; then
+  yum -y remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine
+  yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
-
-yum install -y docker-ce docker-ce-cli containerd.io
+  yum install -y docker-ce docker-ce-cli containerd.io
+elif [[ "${DIST}" == "debian" ]] || [[ "${DIST}" == "ubuntu" ]]; then
+  dpkg --remove docker docker-engine docker.io containerd runc
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  apt -y update
+  apt -y install docker-ce docker-ce-cli containerd.io
+fi
 systemctl enable docker
 systemctl start docker
 
@@ -278,7 +336,7 @@ read -p "Init key: " INITKEY
 read -p "Device name: " DEVICENAME
 
 # Initiate CTS
-sudo docker run --network host \
+docker run --network host \
                 --privileged \
                 --rm \
                 -e "INITIATOR_ENV=eu1" \
